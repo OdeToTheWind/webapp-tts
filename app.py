2f178c6f334c09ef10f244960fa5af7d56589e87
Sadhatri <sandyaru1985@gmail.com>
2023-02-28 16:21:40 +0530

    output is working

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template, jsonify, request
-from flask_restful import Api, Resource
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
-api =   Api(app)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-  
+
+# @app.route('/output') 
+# def output():
+#       return render_template('output.html')
+
+@app.route("/output", methods=['POST'])
+def process():
+    description = request.form['description']
+    language = request.form['language']
+    gender = request.form['gender']
+
+#    return 'Name is: ' + name + ' and the comment is: ' + comment     # This line is also working, but def feunction can return only one task.
+    return render_template('output.html', description=description, language=language, gender=gender)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
